c6f6fe988e5773195169b7a4a8e3b27e95643564
Abhijit Nag <abhijitnag1000@gmail.com>
2023-01-14 14:21:57 +0530

    first update

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Card from "./components/card/Card";
+import Chips from "./components/chip/Chips";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Topbar from "./components/navbar/Topbar";
+import Home from "./pages/home/Home";
+import Partners from "./pages/partners/Partners";
+
+function App() {
+  console.log(`${process.env.REACT_APP_NEWS_API}`)
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/partners" element={<Partners/>}/>
+    </Routes>
+    </BrowserRouter>
+    // <div className="App" style={{display:"flex", flexDirection:"column", alignItems:"center", width:"100vw"}}>
+    //   {/* <h1>Welccome to JMedia.com</h1> */}
+    //   {/* <Topbar/> */}
+    //   {/* <Menu/> */}
+    //   {/* <Chips/> */}
+    //   {/* <Card/> */}
+    //   <Home/>
+    //   <Footer/>
+    // </div>
+  );
+}
+
+export default App;
